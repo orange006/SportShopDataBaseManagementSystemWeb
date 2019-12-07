6a958dc89bc83c60c391948c4b2194afc09ea1bf
Alexey Efimov <alexeyefimov15@gmail.com>
2019-12-07 12:47:13 +0200

    Fixed show feature

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -6,9 +6,9 @@
 
 @section("page-content")
     <h2>Id замовлення: {{ $order->id }}</h2>
-    <h4>Id продукта: {{ $order->IdProd }}</h4>
-    <h4>Id працівника: {{ $order->IdEmpl }}</h4>
-    <h4>Id клієнта: {{ $order->IdCust }}</h4>
+    <h4>Назва продукту: {{ $order->product->NameProduct }}</h4>
+    <h4>ПІБ працівника: {{ $order->employee->FullNameEmployee }}</h4>
+    <h4>ПІБ клієнта: {{ $order->customer->FullNameCustomer }}</h4>
     <h4>Дата замовлення: {{ $order->DateOrder }}</h4>
 
     <a href="/orders" style="margin-top: 30px;" class="btn btn-outline-info">
--- resources/views/supplies/show.blade.php
+++ resources/views/supplies/show.blade.php
@@ -6,8 +6,8 @@
 
 @section("page-content")
     <h2>Id поставки: {{ $supply->id }}</h2>
-    <h4>Id постачальника: {{ $supply->IdProv }}</h4>
-    <h4>Дата: {{ $supply->NameProvider }}</h4>
+    <h4>Назва постачальника: {{ $supply->provider->NameProvider }}</h4>
+    <h4>Дата: {{ $supply->DateSupply }}</h4>
 
     <a href="/supplies" style="margin-top: 30px;" class="btn btn-outline-info">
         Повернутися до списку
